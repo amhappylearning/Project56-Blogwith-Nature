ce2de5a015946e9a7887a582b53c785fa4792554
AM <73906893+amhappylearning@users.noreply.github.com>
2021-04-29 11:49:03 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,6 @@
+function setDate(){
+    document.getElementById('date') = new Date().toDateString();
+}
+    function changecolor(){
+    document.getElementById("color1").innerHTML.style.color="orange";
+    }
